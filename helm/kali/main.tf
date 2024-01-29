dba3cb2c42d5771e6868c0a36b04d6a81c2567f5
Michael <michael.ahli99@gmail.com>
2024-01-29 10:34:47 +0700

    install kali linux for pentesting

---
+++ helm/kali/main.tf
@@ -0,0 +1,18 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "kalivm" {
+  atomic = "true"
+  name = "kalivm"
+  namespace = "hackspace"
+  
+  repository = "https://simonmisencik.github.io/helm-charts"
+  chart = "kali"
+
+  values = [
+    file ("values.yaml"),
+  ]
+}
